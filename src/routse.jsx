4cb4c65ec988089a88bf256340db647eacc5cdea
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2025-05-12 14:38:17 +0330

    create blug page

--- src/routse.jsx
+++ src/routse.jsx
@@ -1,12 +1,15 @@
 import Index from "./Pages"
 import Course from "./Pages/Course"
 import CourseCat from "./Pages/CourseCat"
+import Blug from "./Pages/Blug"
 
 
 const routes = [
 {path: '/' , elements: <Index/>},
 {path: '/course/:courseName' , elements: <Course/>},
 {path: '/course-cat/:categoryName' , elements: <CourseCat/>},
+{path: '/blug/:blugName' , elements: <Blug/>},
+
 
 ]
 
