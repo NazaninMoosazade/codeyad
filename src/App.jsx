f09f8ea30d23b9f790057f71549c95224913a547
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2025-05-12 14:00:40 +0330

    create index page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-green-700 text-xl'>App</div>
+    <>
+    
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        bgBanner:
+          "linear-gradient(270deg, #286bb8, #5fa4f0 23.78%, #286bb6 49.83%, #62a6f3 72.74%, #286bb8)",
+      },
+    },
   },
   plugins: [],
-}
+};
