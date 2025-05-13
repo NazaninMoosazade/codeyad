9a3e64d27135cc403234f66649ea8e9cf31d754f
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2025-05-13 17:41:20 +0330

    create Landing.jsx

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Components/Headers/Header'
+import Landing from '../Components/Landing/Landing'
 
 export default function index() {
   return (
     <>
     <Header/>
+    <Landing/>
     </>
   )
 }
