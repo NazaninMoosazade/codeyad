5489b09a85b02185b3d54cd41e9fe5134309d030
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2025-05-15 00:22:20 +0330

    create SectionTitle

--- src/Components/LastUpdateCourses/LastUpdateCourses.jsx
+++ src/Components/LastUpdateCourses/LastUpdateCourses.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import SectionTitle from '../SectionTitle/SectionTitle'
 
 export default function LastUpdateCourses() {
   return (
-    <div>LastUpdateCourses</div>
+    <div className='w-full max-w-[1600px] mx-auto px-4 lg:px-8'>
+        <SectionTitle title={'آخرین به روز رسانی ها'} btnTitle={'همه دوره ها'} btnHref={''}/>
+    </div>
   )
 }
---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function SectionTitle({ title, btnHref, btnTitle }) {
+  return (
+    <div className="w-full max-w-[1600px] mx-auto px-4 lg:px-8">
+      <div className="flex items-center justify-between">
+        <h3 className="font-DanaDemiBold"> {title} </h3>
+
+        {btnTitle ? (
+          <>
+            {/* Btn  */}
+            <div className="no-underline  h-11 w-28 p-2.5 flex items-center justify-center gap-x-2 text-white bg-blue rounded-lg font-Dana">
+              <Link
+                to={btnHref}
+                className="text-white font-DanaDemiBold no-underline"
+              >
+                {btnTitle}
+              </Link>
+            </div>
+          </>
+        ) : null}
+      </div>
+    </div>
+  );
+}
