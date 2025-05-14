c1f866690fecf2963d093268da4235328b6701d5
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2025-05-15 00:11:32 +0330

    create LastUpdateCourses

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Components/Headers/Header'
 import Landing from '../Components/Landing/Landing'
+import LastUpdateCourses from '../Components/LastUpdateCourses/LastUpdateCourses'
 
 export default function index() {
   return (
     <>
     <Header/>
     <Landing/>
+    <LastUpdateCourses/>
     </>
   )
 }
