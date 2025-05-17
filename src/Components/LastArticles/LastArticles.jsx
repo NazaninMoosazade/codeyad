d78beef747e518b2f07857707b9f39dc7c82f229
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2025-05-17 14:15:37 +0330

    cerate ArticlesCard

--- src/Components/LastArticles/LastArticles.jsx
+++ src/Components/LastArticles/LastArticles.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import SectionTitle from '../SectionTitle/SectionTitle'
+import ArticlesCard from '../ArticlesCard/ArticlesCard'
 
 export default function LastArticles() {
   return (
     <div className='mt-10 w-full max-w-[1600px] mx-auto px-4 lg:px-8'>
         <SectionTitle title={'آخرین مقالات'} btnTitle={'همرو ببین'}/>
+        <div className='w-full max-w-[1600px] mx-auto px-4 lg:px-8 gap-x-2  grid grid-col-1 md:grid-cols-2'>
+         <ArticlesCard/>
+         <ArticlesCard/>
+
+
+        </div>
     </div>
   )
 }
