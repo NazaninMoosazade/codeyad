170c0633fa3d85a2efd449ca81b9cce94354c891
Nazanin Moosazade <moosazadenazanin@gmail.com>
2025-05-22 20:05:21 +0330

    Test green square with correct email

--- src/Pages/AllCourse.jsx
+++ src/Pages/AllCourse.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from '../Components/Headers/Header'
+import Footer from '../Components/Footer/Footer'
 
 export default function () {
   return (
-    <div>AllCourse</div>
+    <>
+    <Header/>
+    <Footer/>
+    </>
   )
 }
